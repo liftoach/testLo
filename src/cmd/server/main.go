97802bfe8cf19a1ce84098eaefa3ae74d122d503
liftoach <petivan@technologysolutionsgeorgia.ge>
2025-08-13 16:44:03 +0300

    added launchServer func in main

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -2,10 +2,14 @@ package server
 
 import (
 	"context"
+	"errors"
+	"log"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -17,4 +21,38 @@ func run() {
 	defer cancel()
 
 	slog.Info("loa")
+}
+
+func launchServer(ctx context.Context, srv *http.Server) (err error) {
+	var shutdownErr error
+
+	defer func() {
+		err = errors.Join(err, shutdownErr)
+	}()
+
+	shutdownDone := make(chan struct{})
+
+	go func() {
+		<-ctx.Done()
+		log.Println("shutting down server...")
+		sdCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		shutdownErr = srv.Shutdown(sdCtx)
+		log.Println("HTTP server stopped")
+
+		close(shutdownDone)
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil
+	default:
+	}
+	log.Printf("Listening on %s", srv.Addr)
+	if err = srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	<-shutdownDone
+	return nil
 }
